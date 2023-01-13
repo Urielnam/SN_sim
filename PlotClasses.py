e6460b06c5df28db8e705f1c6c3034a0aea88db7
Urielnam <yurielnam@gmail.com>
2023-01-13 21:37:20 +0200

    I've done the split between UI and Simulation. you can now run the simulation with ui parameter set to false and it runs (faster...). I've also created the shell that allows the main.py file to run the simultion in Simulation. I've added PlotClasses.py in an attempt to show the different simulations after they are done. it work for 1 simulation but not for multiple simulations.

---
+++ PlotClasses.py
@@ -0,0 +1,89 @@
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+# TODO consider changing this function so it only makes the X and Y axis data and does not create the figure object.
+#  at the parent file the figure object is created and is shown.
+
+import matplotlib.pyplot as plt
+import numpy as np
+from Simulation import data_type_keys
+
+f = plt.figure(figsize=(2, 2), dpi=72)
+a3 = f.add_subplot(121)
+a3.plot()
+a1 = f.add_subplot(222)
+a1.plot()
+a2 = f.add_subplot(224)
+a2.plot()
+
+
+def paintfinal(data_age, data_age_by_type, successful_operations_total, number_of_sensors, agent_flow_rates_by_type,
+               total_resource, self_organization_measure, dt):
+
+    # data_plot = FigureCanvasTkAgg(f, master=main)
+    # data_plot.get_tk_widget().config(height=400)
+    # data_plot.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+
+    # Draw the inital state of the clock and data on the canvas
+
+    # re-draw the clock and data fields on the canvas. Also update the matplotlib charts
+
+    # canvas.delete(self.time)
+
+    # Delete previous data.
+
+    # dt = 5
+
+    a1.set_xlabel("Time")
+    a1.set_ylabel("Average Data Age")
+
+    a1.plot([t for (t, age) in data_age.items()], [np.mean(age) for (t, age) in data_age.items()],
+                label="all")
+
+    for key in data_type_keys:
+        a1.plot([t for (t, age) in data_age_by_type[key].items()],
+                [np.mean(age) for (t, age) in data_age_by_type[key].items()], label = key)
+    a1.legend(loc="upper left")
+
+
+
+    a2.cla()
+    a2.set_xlabel("Time")
+    a2.set_ylabel("Accumulated Success")
+   # code to calculate step function.
+    # successful_operations_total[float(env.now)].append(len([x for x in successful_operations if x > env.now - dt]))
+    a2.plot([t for (t, success) in successful_operations_total.items()],
+            [success for (t, success) in successful_operations_total.items()],
+            label="Average success over last " + str(dt) + " timesteps")
+
+    a3.cla()
+    a3.set_xlabel("Time")
+    a3.set_ylabel("System Cost")
+
+    a3.plot([t for (t, a) in number_of_sensors.items()],
+            [a for (t, a) in number_of_sensors.items()],
+            label="Sensors")
+
+    for key in agent_flow_rates_by_type.keys():
+        a3.plot([t for (t, a) in agent_flow_rates_by_type[key].items()],
+                [a for (t, a) in agent_flow_rates_by_type[key].items()],
+                label=key)
+    a3.plot([t for (t, a) in total_resource.items()],
+            [a for (t, a) in total_resource.items()],
+            label="Total")
+
+    a3.legend(loc="upper left")
+
+    # to calculate self-org, check how much the system has been
+    # "in motion" for the last X timesteps.
+    # sum of number of times the resources were re-allocated.
+
+
+    a2.plot([t for (t, a) in self_organization_measure.items()],
+            [a for (t, a) in self_organization_measure.items()],
+            label="Self-organization effort over last " + str(dt) + " timesteps")
+
+    a2.legend(loc="upper left")
+
+    # data_plot.draw()
+    plt.show()
--- main.py
+++ main.py
@@ -5,5 +5,20 @@ starts file Simulation.py
 """
 
 import Simulation
+import PlotClasses
 
-Simulation.start()
+# TODO create a function to analyze the proportions between self - org and accumulated success.
+# TODO stack it so we can run the simulation multiple times and then show the self-org/accumulated success for each
+#  seperatly and togther
+simulation_collector = Simulation.main_run(False)
+
+
+PlotClasses.paintfinal(simulation_collector[0],
+                           simulation_collector[1],
+                           simulation_collector[2],
+                           simulation_collector[3],
+                           simulation_collector[4],
+                           simulation_collector[5],
+                           simulation_collector[6], dt=5)
+
+# f.show()
